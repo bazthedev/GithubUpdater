60c4128dd9e840221c66b9676a3654f862840cd8
Baz <bazthedev@gmail.com>
2025-02-09 12:42:29 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ reponame = ""
 requirements_file = False
 requirements_file_name = ""
 files = []
+multiplatform = True
+src_code = False
+preinstall = ""
 
 while True:
     username = input("Github Username: ")
@@ -43,6 +46,36 @@ while True:
     if _break:
         break
 
+while True:
+    _mp = input("Does your program support multiple platforms (y/n)? ")
+    if _mp[0].lower() == "y":
+        multiplatform = True
+    else:
+        multiplatform = False
+    confirm = input(f"Is multiplatform={multiplatform} correct? (y/n) ")
+    if confirm[0].lower() == "y":
+        break
+
+while True:
+    _src = input("Does your program use the default source code or an attachment (y/n)? ")
+    if _src[0].lower() == "y":
+        src_code = True
+    else:
+        src_code = False
+    confirm = input(f"Is sourcecode={src_code} correct? (y/n) ")
+    if confirm[0].lower() == "y":
+        break
+
+while True:
+    _preinst = input("Does your program use a preinstall script (y/n)? ")
+    if _preinst[0].lower() == "y":
+        preinstall = input("Please insert the contents of your script or the script file name and/or command to execute it: ")
+    else:
+        preinstall = ""
+    confirm = input(f"Is the following your preinstallation script (y/n)?\n\n{preinstall}")
+    if confirm[0].lower() == "y":
+        break
+
 print("Generating config...")
 
 with open("updater.json", "w") as u:
@@ -52,6 +85,9 @@ with open("updater.json", "w") as u:
     upd["repo"] = reponame
     upd["req_f"] = requirements_file
     upd["req_f_n"] = requirements_file_name
+    upd["multiplatform"] = multiplatform
+    upd["src_code"] = src_code
+    upd["preinstall"] = preinstall
     json.dump(upd, u, indent=4)
 
 updaterdl = requests.get("https://raw.githubusercontent.com/bazthedev/GithubUpdater/main/updater.py")
